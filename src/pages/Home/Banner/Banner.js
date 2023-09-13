5c133ffca5d1ef49d835859cfa4bf03e7e3ad7aa
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-09-13 13:02:27 +0600

    added a home page banner

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import banner1 from '../../../assets/Images/home/banner1.jpg';
+import banner2 from '../../../assets/Images/home/banner2.jpg';
+import banner3 from '../../../assets/Images/home/banner3.png';
+import banner4 from '../../../assets/Images/home/banner4.jpg';
+import banner5 from '../../../assets/Images/home/banner5.png';
+import banner6 from '../../../assets/Images/home/banner6.png';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <section>
+            <Carousel>
+                <div>
+                    <img src={banner1} alt='banner' />
+                </div>
+                <div>
+                    <img src={banner2} alt='banner' />
+                </div>
+                <div>
+                    <img src={banner3} alt='banner' />
+                </div>
+                <div>
+                    <img src={banner4} alt='banner' />
+                </div>
+                <div>
+                    <img src={banner5} alt='banner' />
+                </div>
+                <div>
+                    <img src={banner6} alt='banner' />
+                </div>
+            </Carousel>
+        </section>
+    );
+};
+
+export default Banner;
--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/Images/others/logo.png';
 
 const Navbar = () => {
+
+    const navOptions = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contact">Contact Us</Link></li>
+        <li><Link to="/login">Login</Link></li>
+    </>
+
     return (
-        <div>
-            <h1>This is navbar!!</h1>
+        <div style={{background: "rgba(21, 21, 21, 0.50)"}} className=' fixed z-10 w-full text-white'>
+            <div className="navbar container mx-auto max-w-screen-xl">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label htmlFor='' tabIndex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul style={{background: "rgba(21, 21, 21, 0.80)"}} tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-56">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <Link to="/">
+                        <img className='w-full' style={{ height: "40px" }} src={logo} alt="Logo" />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex items-center">
+                    <ul className="flex items-center menu menu-horizontal p-0 text-white">
+                        {navOptions}
+                    </ul>
+                </div>
+                {/* <div className="navbar-end lg:hidden">
+                    <label tabIndex="1" htmlFor="dashboard-sidebar" className="btn btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                </div> */}
+            </div>
         </div>
     );
 };
