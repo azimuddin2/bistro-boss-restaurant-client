4ce6a6627121f8d09fda7f294f65fd0c01419b30
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-01 22:20:54 +0600

    added add item and use react-form

--- src/pages/Dashboard/AdminPages/AddItem/AddItem.js
+++ src/pages/Dashboard/AdminPages/AddItem/AddItem.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import SectionTitle from '../../../../components/SectionTitle/SectionTitle';
+import { useForm } from 'react-hook-form';
+import { MdOutlineErrorOutline } from 'react-icons/md';
+import { IoRestaurant } from 'react-icons/io5';
+import Button from '../../../../components/Button/Button';
 
 const AddItem = () => {
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+
     return (
-        <div>
-            <h1 className='text-4xl'>Add Item!!</h1>
-        </div>
+        <section className='my-10'>
+            <SectionTitle subHeading={"What's New?"} heading={'Add an Item'}></SectionTitle>
+            <div className='p-8 w-4/5 mx-auto bg-[#F3F3F3]'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold">Recipe Name*</span>
+                        </label>
+                        <input
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: 'Recipe Name is required',
+                                }
+                            })}
+                            type='text'
+                            placeholder="Recipe name"
+                            className="input rounded-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                        />
+                        <label className="label pt-1">
+                            {errors.name?.type === 'required' && <span className="label-text-alt text-red-500 text-sm flex items-center"><MdOutlineErrorOutline className='text-lg' style={{ marginRight: '2px' }}></MdOutlineErrorOutline>{errors.name.message}</span>}
+                        </label>
+                    </div>
+
+                    <Button>Add Item <IoRestaurant className='text-xl animate-pulse'></IoRestaurant></Button>
+                </form>
+            </div>
+        </section>
     );
 };
 
