00abf0c9710eebab828b141427b3c1a138e0b0c3
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-03 22:52:31 +0600

    updated payment and checkoutForm

--- src/pages/Dashboard/UserPages/Payment/CheckoutForm.js
+++ src/pages/Dashboard/UserPages/Payment/CheckoutForm.js
@@ -1,5 +1,6 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const CheckoutForm = () => {
     const stripe = useStripe();
@@ -15,6 +16,19 @@ const CheckoutForm = () => {
         const card = elements.getElement(CardElement);
         if (card === null) {
             return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            console.log('error', error);
+            toast.error(error.message);
+        }
+        else {
+            console.log('payment method', paymentMethod)
         }
 
     };
@@ -37,7 +51,11 @@ const CheckoutForm = () => {
                     },
                 }}
             />
-            <button type="submit" disabled={!stripe}>
+            <button
+                className='btn btn-primary btn-sm rounded-sm mt-5 text-white'
+                type="submit"
+                disabled={!stripe}
+            >
                 Pay
             </button>
         </form>
--- src/pages/Dashboard/UserPages/Payment/Payment.js
+++ src/pages/Dashboard/UserPages/Payment/Payment.js
@@ -8,9 +8,9 @@ const stripePromise = loadStripe(process.env.REACT_APP_Payment_Gateway_PK);
 
 const Payment = () => {
     return (
-        <section className='my-10'>
+        <section className='bg-[#F6F6F6] py-12 h-screen'>
             <SectionTitle subHeading={'Please process'} heading={'Payment'}></SectionTitle>
-            <div>
+            <div className='w-11/12 lg:w-1/2 mx-auto bg-white p-5 lg:p-10'>
                 <Elements stripe={stripePromise}>
                     <CheckoutForm></CheckoutForm>
                 </Elements>
