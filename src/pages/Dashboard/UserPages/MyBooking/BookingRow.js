e5a882138e2b6f189e0d40230a1237f908d572a4
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-10 23:35:44 +0600

    complete my booking

--- src/pages/Dashboard/UserPages/MyBooking/BookingRow.js
+++ src/pages/Dashboard/UserPages/MyBooking/BookingRow.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
 const BookingRow = ({ index, booking }) => {
-    const { email, phone, guest, date, time, status } = booking;
+    const { email, guest, date, time, status } = booking;
 
     return (
         <tr className='text-secondary font-medium'>
             <td>{index + 1}</td>
             <td>{email}</td>
-            <td>{phone}</td>
             <td>{guest}</td>
             <td>{date.slice(0, 10)}</td>
-            <td className='w-full'>{time}</td>
-            <td><span className="loading loading-spinner loading-xs">{status}</span></td>
+            <td>{time}</td>
+            <td className='flex items-center'>
+                {
+                    status === "success" ?
+                        <>
+                            <span className='badge bg-green-600 badge-xs mr-1'></span>
+                            <span className='capitalize'>{status}</span>
+                        </>
+                        :
+                        <>
+                            <span className="loading loading-spinner loading-xs mr-1 text-green-500"></span>
+                            <span className='capitalize'>{status}</span>
+                        </>
+                }
+            </td>
         </tr>
     );
 };
--- src/pages/Dashboard/UserPages/MyBooking/MyBooking.js
+++ src/pages/Dashboard/UserPages/MyBooking/MyBooking.js
@@ -6,6 +6,10 @@ import { useQuery } from '@tanstack/react-query';
 import Error from '../../../Shared/Error/Error';
 import Loading from '../../../Shared/Loading/Loading';
 import BookingRow from './BookingRow';
+import bookingImg from '../../../../assets/Images/others/authentication1.png';
+import Button from '../../../../components/Button/Button';
+import { Link } from 'react-router-dom';
+import { TbCalendarShare } from 'react-icons/tb';
 
 const MyBooking = () => {
     const { user, loading } = useAuth();
@@ -29,38 +33,49 @@ const MyBooking = () => {
     }
 
     return (
-        <section className='bg-[#F6F6F6] pb-20 p-10 h-full'>
-            <SectionTitle subHeading={'Excellent Ambience'} heading={'My Bookings'}></SectionTitle>
-            <div className='w-11/12 lg:w-4/5 mx-auto bg-white p-5 lg:p-10'>
-                <div className='flex justify-between items-center mb-6'>
-                    <h2 className='lg:text-xl uppercase font-bold font-family text-secondary'>Total Bookings: {bookings?.length}</h2>
-                </div>
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        <thead className='bg-[#D1A054] text-white font-family uppercase'>
-                            <tr className='font-normal'>
-                                <th></th>
-                                <th>Email</th>
-                                <th>Phone Number</th>
-                                <th>Guest Member</th>
-                                <th>Booking Date</th>
-                                <th>Booking Time</th>
-                                <th>Activity</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                bookings?.map((booking, index) => <BookingRow
-                                    key={booking._id}
-                                    index={index}
-                                    booking={booking}
-                                ></BookingRow>)
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </section>
+        <>
+            {
+                bookings.length > 0 ?
+                    <div className='bg-[#F6F6F6] pb-20 p-10 h-full'>
+                        <SectionTitle subHeading={'Excellent Ambience'} heading={'My Bookings'}></SectionTitle>
+                        <div className='w-11/12 lg:w-5/6 mx-auto bg-white p-5 lg:p-10'>
+                            <div className='flex justify-between items-center mb-6'>
+                                <h2 className='lg:text-xl uppercase font-bold font-family text-secondary'>Total Bookings: {bookings?.length}</h2>
+                            </div>
+                            <div className="overflow-x-auto">
+                                <table className="table">
+                                    <thead className='bg-[#D1A054] text-white font-family uppercase'>
+                                        <tr className='font-normal'>
+                                            <th></th>
+                                            <th>Email</th>
+                                            <th>Guest Member</th>
+                                            <th>Booking Date</th>
+                                            <th>Booking Time</th>
+                                            <th>Activity</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            bookings?.map((booking, index) => <BookingRow
+                                                key={booking._id}
+                                                index={index}
+                                                booking={booking}
+                                            ></BookingRow>)
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <div className='w-full lg:w-3/5 mx-auto my-10'>
+                        <img src={bookingImg} alt="" className='w-full' />
+                        <Link to={'/dashboard/reservation'} className='flex justify-center mt-2'>
+                            <Button>Book A Table <TbCalendarShare className='text-xl'></TbCalendarShare></Button>
+                        </Link>
+                    </div>
+            }
+        </>
     );
 };
 
