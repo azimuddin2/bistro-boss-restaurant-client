cab256aa12882c277f7836364e9b3a0ca018353c
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-11 10:47:29 +0600

    added manage bookings

---
+++ src/pages/Dashboard/AdminPages/ManageBookings/ManageBookingRow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageBookingRow = ({index, booking, refetch}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ManageBookingRow;
