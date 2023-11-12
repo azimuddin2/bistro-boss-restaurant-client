24bb7eec8d00d509ccec3e9d314d7bf50ebd1c51
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-12 22:22:16 +0600

    updated manage booking

--- src/pages/Dashboard/AdminPages/ManageBookings/ManageBookingRow.js
+++ src/pages/Dashboard/AdminPages/ManageBookings/ManageBookingRow.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { GoCheckCircleFill } from 'react-icons/go';
 
-const ManageBookingRow = ({index, booking, refetch}) => {
+const ManageBookingRow = ({ index, booking, refetch }) => {
+    const { name, email, phone, guest, date, time, status } = booking;
+
     return (
-        <div>
-            
-        </div>
+        <tr className='text-secondary font-medium'>
+            <td>{index + 1}</td>
+            <td>
+                {name}
+                <br />
+                <span className="badge badge-ghost badge-sm">{email}</span>
+            </td>
+            <td>{phone}</td>
+            <td>{guest}</td>
+            <td>
+                {date.slice(0, 10)}
+                <br />
+                <span className="badge badge-ghost badge-sm">{time}</span>
+            </td>
+            <td>
+                {
+                    status === "success" ?
+                        <span className='capitalize text-green-600 font-bold'>{status}</span>
+                        :
+                        <span className='capitalize text-primary font-bold'>{status}</span>
+                }
+            </td>
+
+            <td>
+                {
+                    status === "success" ?
+                        <GoCheckCircleFill className='text-4xl text-green-700'></GoCheckCircleFill>
+                        :
+                        <GoCheckCircleFill className='text-4xl text-green-300 cursor-pointer'></GoCheckCircleFill>
+                }
+            </td>
+        </tr>
     );
 };
 
--- src/pages/Dashboard/AdminPages/ManageBookings/ManageBookings.js
+++ src/pages/Dashboard/AdminPages/ManageBookings/ManageBookings.js
@@ -29,7 +29,7 @@ const ManageBookings = () => {
         <section className='bg-[#F6F6F6] pb-20 p-10 h-full'>
             <SectionTitle subHeading={"At a Glance!"} heading={"Manage All Bookings"}></SectionTitle>
             <div className='w-11/12 lg:w-5/6 mx-auto bg-white p-5 lg:p-10'>
-                <div className='flex justify-between items-center mb-6'>
+                <div className='flex justify-between items-center mb-3'>
                     <h2 className='lg:text-xl uppercase font-bold font-family text-secondary'>All Bookings: {bookings?.length}</h2>
                 </div>
                 <div className="overflow-x-auto">
@@ -38,6 +38,7 @@ const ManageBookings = () => {
                             <tr className='font-normal'>
                                 <th></th>
                                 <th>Name & Email</th>
+                                <th>Phone Number</th>
                                 <th>Guest Member</th>
                                 <th>Booking Date & Time</th>
                                 <th>Activity</th>
