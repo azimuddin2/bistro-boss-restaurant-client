237f81cec5c391f981dd5d70c4124a846d64c95a
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-13 19:16:02 +0600

    complete manage booking task

--- src/pages/Dashboard/AdminPages/ManageBookings/ManageBookingRow.js
+++ src/pages/Dashboard/AdminPages/ManageBookings/ManageBookingRow.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { GoCheckCircleFill } from 'react-icons/go';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import swal from 'sweetalert';
 
 const ManageBookingRow = ({ index, booking, refetch }) => {
+    const [axiosSecure] = useAxiosSecure();
     const { _id, name, email, phone, guest, date, time, status } = booking;
 
-    const handleStatusUpdate = () => {
-
+    const handleStatusUpdate = (id) => {
+        axiosSecure.patch(`/bookings/${id}`, { status: 'success' })
+            .then(result => {
+                if(result.data.modifiedCount){
+                    swal({
+                        title: "Status updated successfully",
+                        icon: "success",
+                        timer: 3000,
+                    });
+                    refetch();
+                }
+            })
     };
 
     return (
