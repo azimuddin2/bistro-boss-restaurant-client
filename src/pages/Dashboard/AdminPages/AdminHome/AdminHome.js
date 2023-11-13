f333e40fc388b04b5fad1efcabced9b7fabd3538
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-13 23:48:34 +0600

    added admin home

--- src/pages/Dashboard/AdminPages/AdminHome/AdminHome.js
+++ src/pages/Dashboard/AdminPages/AdminHome/AdminHome.js
@@ -1,10 +1,71 @@
 import React from 'react';
+import useAuth from '../../../../hooks/useAuth';
+import { IoFastFood } from 'react-icons/io5';
+import { RiLuggageCartFill } from 'react-icons/ri';
+import { FaUsers, FaSackDollar } from 'react-icons/fa6';
 
 const AdminHome = () => {
+    const { user } = useAuth();
+
     return (
-        <div>
-            <h1 className='text-4xl'>Admin Home!!</h1>
-        </div>
+        <section className='h-full bg-[#F6F6F6] px-5 lg:px-10'>
+            <h1 className='lg:text-xl uppercase font-bold font-family text-secondary'>Hello, Welcome {user.displayName}</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3'>
+
+                <div
+                    style={{ background: 'linear-gradient(90deg, #BB34F5 0%, #FCDBFF 100%)' }}
+                    className='card lg:card-side pt-6 lg:pt-0 rounded-lg text-white'
+                >
+                    <figure className='w-16'>
+                        <FaSackDollar className='lg'></FaSackDollar>
+                    </figure>
+                    <div className="card-body pt-4 lg:pt-8">
+                        <h2 className="card-title justify-center lg:justify-start">{0}</h2>
+                        <p className='text-center lg:text-left text-lg'>Revenue</p>
+                    </div>
+                </div>
+
+                <div
+                    style={{ background: 'linear-gradient(90deg, #D3A256 0%, #FDE8C0 100%)' }}
+                    className='card lg:card-side pt-6 lg:pt-0 rounded-lg text-white'
+                >
+                    <figure className='w-16'>
+                        <FaUsers className='text-xl'></FaUsers>
+                    </figure>
+                    <div className="card-body pt-4 lg:pt-8">
+                        <h2 className="card-title justify-center lg:justify-start">{0}</h2>
+                        <p className='text-center lg:text-left text-lg'>Users</p>
+                    </div>
+                </div>
+
+                <div
+                    style={{ background: 'linear-gradient(90deg, #FE4880 0%, #FECDE9 100%)' }}
+                    className='card lg:card-side pt-6 lg:pt-0 rounded-lg text-white'
+                >
+                    <figure className='w-16'>
+                        <IoFastFood className='text-xl'></IoFastFood>
+                    </figure>
+                    <div className="card-body pt-4 lg:pt-8">
+                        <h2 className="card-title justify-center lg:justify-start">{0}</h2>
+                        <p className='text-center lg:text-left text-lg'>Menu Item</p>
+                    </div>
+                </div>
+
+                <div
+                    style={{ background: 'linear-gradient(90deg, #6AAEFF 0%, #B6F7FF 100%)' }}
+                    className='card lg:card-side pt-6 lg:pt-0 rounded-lg text-white'
+                >
+                    <figure className='w-16'>
+                        <RiLuggageCartFill className='text-xl'></RiLuggageCartFill>
+                    </figure>
+                    <div className="card-body pt-4 lg:pt-8">
+                        <h2 className="card-title justify-center lg:justify-start">{0}</h2>
+                        <p className='text-center lg:text-left text-lg'>Orders</p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     );
 };
 
--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/Images/others/logo.png';
 import CustomLink from './CustomLink';
-import { AuthContext } from '../../../providers/AuthProvider';
 import swal from 'sweetalert';
 import './Navbar.css';
 import useCart from '../../../hooks/useCart';
+import useAuth from '../../../hooks/useAuth';
+import useAdmin from '../../../hooks/useAdmin';
 
 const Navbar = () => {
-    const { user, logout } = useContext(AuthContext);
+    const { user, logout } = useAuth();
+    const [isAdmin] = useAdmin();
     const [carts] = useCart();
 
     const handleLogout = () => {
@@ -29,7 +31,16 @@ const Navbar = () => {
         <li><CustomLink to="/menu">Our Menu</CustomLink></li>
         <li><CustomLink to="/order/coffee">Order Food</CustomLink></li>
         <li><CustomLink to="/contact">Contact Us</CustomLink></li>
-        {/* <li><CustomLink to="/dashboard">Dashboard</CustomLink></li> */}
+        {
+            user?.uid && <li>
+                {
+                    isAdmin ?
+                        <CustomLink to="/dashboard/admin-home">Dashboard</CustomLink>
+                        :
+                        <CustomLink to="/dashboard/user-home">Dashboard</CustomLink>
+                }
+            </li>
+        }
         <li>
             {
                 user?.uid ?
@@ -66,12 +77,15 @@ const Navbar = () => {
                 </div>
                 {
                     user?.uid && <div className="navbar-end-modify flex items-center ml-3">
-                        <Link to="/dashboard/my-cart">
-                            <div className="indicator cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span className="badge badge-sm indicator-item bg-primary border-none text-white">{carts?.length || 0}</span>
-                            </div>
-                        </Link>
+
+                        {
+                            !isAdmin && <Link to="/dashboard/my-cart">
+                                <div className="indicator cursor-pointer">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                                    <span className="badge badge-sm indicator-item bg-primary border-none text-white">{carts?.length || 0}</span>
+                                </div>
+                            </Link>
+                        }
 
                         <label tabIndex="1" htmlFor="dashboard-sidebar" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
