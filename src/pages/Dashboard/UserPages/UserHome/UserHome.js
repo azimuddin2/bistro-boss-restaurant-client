3b4a149e61715a05ad2eac11cc70fc016d18dbb9
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-22 11:02:35 +0000

    updated userHome page

--- src/pages/Dashboard/UserPages/UserHome/UserHome.js
+++ src/pages/Dashboard/UserPages/UserHome/UserHome.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import useAuth from '../../../../hooks/useAuth';
 import CountUp from 'react-countup';
-import { FaCalendarAlt, FaShopify } from "react-icons/fa";
+import { FaCalendarAlt, FaLuggageCart, FaShopify, FaStar } from "react-icons/fa";
 import { FaMoneyCheckDollar } from "react-icons/fa6";
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import Error from '../../../Shared/Error/Error';
 import Loading from '../../../Shared/Loading/Loading';
+import { GiMoneyStack } from "react-icons/gi";
+import { BiEdit } from "react-icons/bi";
+import userImg from '../../../../assets/Images/others/user.png';
+import { Link } from 'react-router-dom';
+import Button from '../../../../components/Button/Button';
 
 const UserHome = () => {
     const { user } = useAuth();
@@ -31,8 +36,8 @@ const UserHome = () => {
     }
 
     return (
-        <section className='h-full bg-[#F6F6F6] px-5 lg:px-10'>
-            <h1 className='lg:text-2xl uppercase font-bold font-family text-secondary pt-10'>Hi, Welcome {user.displayName}!</h1>
+        <section className='h-full bg-[#F6F6F6] px-5 lg:px-10 mb-12'>
+            <h1 className='text-2xl lg:text-left text-center uppercase font-bold font-family text-secondary pt-10'>Hi, Welcome Back!</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-6'>
                 <div
                     style={{ background: 'linear-gradient(90deg, #BB34F5 0%, #FCDBFF 100%)' }}
@@ -78,7 +83,41 @@ const UserHome = () => {
                 </div>
             </div>
 
-
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-3'>
+                <div className='bg-[#FFEDD5] text-center px-8 py-12'>
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-1">
+                            {
+                                user.photoURL ?
+                                    <img src={user.photoURL} alt='' className='w-full rounded-full' />
+                                    :
+                                    <img src={userImg} alt='' className='w-full rounded-full' />
+                            }
+                        </div>
+                    </div>
+                    <h1 className='text-2xl font-medium font-family my-3'>{user.displayName}</h1>
+                    <Link to={'/edit-profile'}>
+                        <Button>Edit Profile <BiEdit className='text-xl' /></Button>
+                    </Link>
+                </div>
+                <div className='bg-[#FEF9C3] py-16 px-16'>
+                    <p className='text-2xl font-family font-semibold text-secondary mb-4'>Your Activities</p>
+                    <div>
+                        <p className='flex items-center text-[#00C4A1] mb-1'>
+                            <FaLuggageCart className='text-lg' />
+                            <span className='font-family font-medium ml-1'>Orders: {payment}</span>
+                        </p>
+                        <p className='flex items-center text-[#FFBB28] mb-1'>
+                            <FaStar className='text-lg' />
+                            <span className='font-family font-medium ml-1'>Review: {review}</span>
+                        </p>
+                        <p className='flex items-center text-[#FF8042] mb-1'>
+                            <GiMoneyStack className='text-lg' />
+                            <span className='font-family font-medium ml-1'>Payment: {payment}</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
