812537c665a56ee3ba71e2eba76e709b1529d42d
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-12-01 11:22:55 +0000

    added manageBooking search query

--- src/pages/Dashboard/AdminPages/ManageBookings/ManageBookings.js
+++ src/pages/Dashboard/AdminPages/ManageBookings/ManageBookings.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import SectionTitle from '../../../../components/SectionTitle/SectionTitle';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
@@ -7,14 +7,22 @@ import Loading from '../../../Shared/Loading/Loading';
 import ManageBookingRow from './ManageBookingRow';
 import { useLoaderData } from 'react-router-dom';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { IoSearch } from "react-icons/io5";
 
 const ManageBookings = () => {
     const [axiosSecure] = useAxiosSecure();
     const { totalBookings } = useLoaderData();
     const [currentPage, setCurrentPage] = useState(0);
     const [bookingsPerPage, setBookingsPerPage] = useState(5);
+    const searchRef = useRef();
+    const [search, setSearch] = useState('');
 
     const totalPages = Math.ceil(totalBookings / bookingsPerPage);
+
+    const handleSearch = () => {
+        setSearch(searchRef.current.value);
+        setCurrentPage(currentPage);
+    };
 
     const handlePrevClick = () => {
         if (currentPage > 0) {
@@ -29,9 +37,9 @@ const ManageBookings = () => {
     };
 
     const { data: bookings, isLoading, error, refetch } = useQuery({
-        queryKey: ['all-bookings', currentPage, bookingsPerPage],
+        queryKey: ['all-bookings', currentPage, bookingsPerPage, search],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/all-bookings?page=${currentPage}&limit=${bookingsPerPage}`);
+            const res = await axiosSecure.get(`/all-bookings?page=${currentPage}&limit=${bookingsPerPage}&search=${search}`);
             return res.data;
         }
     });
@@ -48,8 +56,30 @@ const ManageBookings = () => {
         <section className='bg-[#F6F6F6] py-10 lg:p-10 h-full'>
             <SectionTitle subHeading={"At a Glance!"} heading={"Manage All Bookings"}></SectionTitle>
             <div className='w-11/12 lg:w-5/6 mx-auto bg-white p-5 lg:p-10'>
-                <div className='flex justify-between items-center mb-3'>
+                <div className='lg:flex justify-between items-center mb-3 lg:mb-5'>
                     <h2 className='text-lg lg:text-xl uppercase font-bold font-family text-secondary'>All Bookings: {totalBookings}</h2>
+                    
+                  
+                    
+                    <div className="join w-full lg:w-72 my-2 lg:my-0">
+                        <input
+                            type='text'
+                            ref={searchRef}
+                            className="input input-sm w-full rounded input-bordered join-item focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                            placeholder="Search booking..."
+                        />
+                        <button
+                            className="btn btn-sm join-item rounded bg-primary hover:bg-primary text-white"
+                            onClick={handleSearch}
+                        >
+                            <IoSearch className='text-xl' />
+                        </button>
+                    </div>
+
+                    {
+                        search && <h2 className='text-lg font-family text-right'>Matching Results: {bookings.length}</h2>
+                    }
+
                 </div>
                 <div className="overflow-x-auto">
                     <table className="table">
